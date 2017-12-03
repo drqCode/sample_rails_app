c1f2e1f2c48217c3f12bafa691845e72dc846abe
Constantin Capatina <constantin.sorin.capatina@gmail.com>
2017-12-03 11:34:14 +0200

    improved user model for defining activation attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save {email.downcase!}
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: {maximum: 54}
 
@@ -41,6 +42,13 @@ class User < ApplicationRecord
     end
   end
 
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
 
-
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
