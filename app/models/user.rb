95d03855a4c6fe453315756be496b5d9766a2d8f
Constantin Capatina <constantin.sorin.capatina@gmail.com>
2017-12-05 21:36:21 +0200

    created relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship',
+                                  foreign_key: 'follower_id',
+                                  dependent:   :destroy
 
   has_secure_password
 
