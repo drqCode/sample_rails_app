1080ba05c172bc994e3e6f4aec525a4580277d31
Constantin Capatina <constantin.sorin.capatina@gmail.com>
2017-12-06 11:46:27 +0200

    following/followers page for user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,7 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:tet)
-    @user2=users(:denis)
+    @user2 = users(:denis)
   end
 
   test 'should redirect index when not logged in' do
@@ -44,6 +44,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+
+  test 'should redirect following when not logged in' do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect followers when not logged in' do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 
 end
